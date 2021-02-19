dec979df62ddddf6a2b365066043d13008f01586
GrafBaton <79304834+GrafBaton@users.noreply.github.com>
2021-02-19 12:59:57 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ $(function(){
         slidesToShow:3,
     });
 });
+
+document.addEventListener("DOMContentLoaded", function(){
+  var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
+  console.log(scrollbar);
+  document.querySelector('[href="#openModal"]').addEventListener('click',function(){
+    document.body.style.overflow = 'hidden';
+    document.querySelector('#openModal').style.marginLeft = scrollbar;
+  });
+  document.querySelector('[href="#close"]').addEventListener('click',function(){
+    document.body.style.overflow = 'visible';
+    document.querySelector('#openModal').style.marginLeft = '0px';
+  });
+});
